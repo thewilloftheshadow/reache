ffbcf914e99364e00b8e418f943b96d1e735950c
thewilloftheshadow <william.shadow@outlook.com>
2021-07-31 18:31:00 +0000

    update 1

--- items.js
+++ items.js
@@ -5,7 +5,7 @@ const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 4)
 const url = `${config.root}/api/items`
 
 const master = {
-  adult: ["XS", "S", "M", "L", "XL", {size: "2XL", price: 2.25}],
+  adult: ["XS", "S", "M", "L", "XL"],
   youth: ["S", "M", "L", "XL"],
   baby: ["3-6M", "6-12M", "12-18M", "18-24M"],
   toddler: ["2T", "3T", "4T", "5T"],
@@ -14,44 +14,37 @@ const master = {
 const allitems = {
   crewneck: {
     name: "Crew Neck",
-    price: 7.5,
+    price: 8,
     sizes: {
-      adult: master.adult.concat([{size: "4XL", price: 2.25}]),
       youth: master.youth,
+      adult: master.adult.concat([, { size: "2XL", price: 2.25 }, { size: "3XL", price: 2.25 }, { size: "4XL", price: 2.25 }]),
     },
   },
-  hoodie: {
-    name: "Pullover Hoodie",
-    price: 23.5,
+  vneck: {
+    name: "V-Neck - Adult",
+    price: 11.25,
     sizes: {
-      adult: master.adult
+      adult: master.adult.concat([{ size: "3XL", price: 2.25 }]),
     },
+  },
+  toddler: {
+    name: "Crew Neck - Toddler",
+    price: 8,
+    sizes: {
+      other: master.toddler,
+    },
+  },
+  babycrew: {
+    name: "Crew Neck - Baby",
+    price: 8,
+    sizes: { other: master.baby },
   },
   longsleeves: {
     name: "Long Sleeves",
     price: 11.25,
     sizes: {
-      adult: master.adult,
       youth: ap(master.youth, "XL"),
-    },
-  },
-  onesie: {
-    name: "Baby Onesie",
-    price: 7.5,
-    sizes: { other: master.baby },
-  },
-  toddler: {
-    name: "Toddler",
-    price: 7.50,
-    sizes: {
-      other: master.toddler,
-    },
-  },
-  vneck: {
-    name: "V-Neck",
-    price: 11.25,
-    sizes: {
-      adult: master.adult.concat([{size: "3XL", price: 2.25}])
+      adult: master.adult,
     },
   },
   zippered: {
@@ -61,6 +54,17 @@ const allitems = {
       adult: master.adult,
       youth: ap(master.youth, "XL"),
     },
+  },
+  hoodie: {
+    name: "Pullover Hoodie - Adult",
+    price: 23.5,
+    sizes: {
+      adult: master.adult,
+    },
+  },
+  hat: {
+    name: "Hat",
+    price: 20,
   },
 }
 
@@ -74,34 +78,36 @@ let generate = async function () {
       url,
       image: `${config.root}/images/${x}.png`,
     }
-    if (itemsrc.sizes.adult) {
+    if (itemsrc.sizes?.adult) {
       let pushitem = { ...item }
       pushitem.name += itemsrc.sizes.youth ? ` - Adult` : ""
       pushitem.id = `${x}-adult`
       pushitem.sizefield = sizelist(itemsrc.sizes.adult)
-      module.exports.push(pushitem)
     }
-    if (itemsrc.sizes.other) {
+    if (itemsrc.sizes?.other) {
       let pushitem = { ...item }
       pushitem.id = `${x}`
       pushitem.sizefield = sizelist(itemsrc.sizes.other)
-      module.exports.push(pushitem)
     }
-    if (itemsrc.sizes.youth) {
+    if (itemsrc.sizes?.youth) {
       let pushitem = { ...item }
       pushitem.name += itemsrc.sizes.adult ? ` - Youth` : ""
       pushitem.id = `${x}-youth`
       pushitem.sizefield = sizelist(itemsrc.sizes.youth)
-      module.exports.push(pushitem)
+    }
+    if (!itemsrc.sizes) {
+      let pushitem = {...item}
+      pushitem.id = `${x}`
+      module.exports.push(itemsrc)
     }
   }
 }
 
 const sizelist = (list) => {
   let z = []
-  list.forEach(x => {
-    if(typeof x == "string") z.push(x)
-    if(typeof x == "object") z.push(`${x.size}[+${x.price}]`)
+  list.forEach((x) => {
+    if (typeof x == "string") z.push(x)
+    if (typeof x == "object") z.push(`${x.size}[+${x.price}]`)
   })
   return z.join("|")
 }
